23a1e5b48496bed37d45ae7e211e0d34d95f7397
KHUSHI 444 <khankhushi4452@gmail.com>
2024-10-29 21:41:13 +0530

    Api fetching

--- src/components/UserProvider.jsx
+++ src/components/UserProvider.jsx
@@ -1,2 +1,20 @@
 import {  useState } from "react";
 import axios from "axios";
+
+
+export const UserContext = createContext();
+
+function UserProvider({ children }) {
+ const [users, setUsers] = useState([])
+ useEffect(() => {
+const fetchApiData = async () => {
+ try {
+ const response = await axios.get(
+ "https://mocki.io/v1/a6a0fb6b-a84a-4934-b3f2-5c92cc77c44e"
+ );
+ setUsers(response.data); // Set the fetched user data
+ console.log("Fetched users:", response.data); // Log fetched users
+} catch (error) {
+ console.log("Error fetching the user data", error);
+  }
+};
